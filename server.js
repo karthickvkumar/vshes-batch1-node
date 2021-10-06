e6ffed6f260953cd8e1848258dc39255e83ae776
kvelraj_concord <kvelraj@concord.net>
2021-10-06 10:38:16 +0530

    get

--- server.js
+++ server.js
@@ -7,7 +7,24 @@ const http = require('http').createServer(app);
 app.use(bodyParser.json());
 app.use(express.json());
 
+const userList = [
+  {
+    name : "Karthick",
+    age : 27
+  },
+  {
+    name : "Kumar",
+    age : 28
+  },
+  {
+    name : "KK",
+    age : 29
+  },
+]
 
+app.get("/api/users", (request, response) => {
+  response.status(200).send(userList);
+});
 
 
 const port = process.env.PORT || 8080;
