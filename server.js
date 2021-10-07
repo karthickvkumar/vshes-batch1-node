f633347e66b9a5d9d8fc7c29c09022f18da0b1f0
kvelraj_concord <kvelraj@concord.net>
2021-10-07 10:16:25 +0530

    post method

--- server.js
+++ server.js
@@ -10,22 +10,51 @@ app.use(express.json());
 const userList = [
   {
     name : "Karthick",
-    age : 27
+    age : 27,
+    id : 1
   },
   {
     name : "Kumar",
-    age : 28
-  },
-  {
-    name : "KK",
-    age : 29
-  },
+    age : 28,
+    id : 2
+  }
 ]
 
-app.get("/api/users", (request, response) => {
+app.get("/api/user/list", (request, response) => {
   response.status(200).send(userList);
 });
 
+app.post("/api/user/create", (request, response) => {
+  let user = request.body;
+  user.id = userList.length + 1;
+
+  userList.push(user);
+  response.status(200).send({
+    message : "Successfully created the user profile"
+  })
+});
+
+app.put("/api/user/edit/:id", (request, response) => {
+  let id = request.params.id;
+  let index = userList.findIndex((value) => {
+    return value.id == id;
+  });
+
+  if(index < 0){
+    response.status(401).send({
+      message : "Invalid ID number"
+    });
+    return;
+  }
+
+  userList[index].name = request.body.name;
+  userList[index].age = request.body.age;
+  
+  response.status(200).send({
+    message : "Successfully Updated the user information"
+  })
+
+});
 
 const port = process.env.PORT || 8080;
 http.listen(port, () => {
