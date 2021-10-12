41c0884a08d0c6fdc50eaf85ed64563d2ad52f7d
kvelraj_concord <kvelraj@concord.net>
2021-10-12 10:50:24 +0530

    sql

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.json());
 const connection = mysql.createConnection({
   host : "localhost",
   user : "root",
-  password : ""
+  password : "",
+  database : "vshesh"
 })
 
 connection.connect((error) => {
@@ -22,15 +23,60 @@ connection.connect((error) => {
 });
 
 app.get("/api/user/list", (request, response) => {
-  
+  const query = `SELECT * FROM user_information`;
+
+  connection.query(query, (error, result) => {
+      if(error){
+        response.status(500).send(error);
+        return;
+      }
+
+      response.status(200).send(result)
+  });
 });
 
 app.post("/api/user/create", (request, response) => {
-  
+  const firstName = request.body.first_name;
+  const lastName = request.body.last_name;
+  const email = request.body.email;
+  const contactNumber = request.body.contact_number;
+
+  const query = `INSERT INTO user_information (first_name, last_name, email, contact_number) VALUES ('${firstName}', '${lastName}', '${email}', ${contactNumber})`;
+
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send({
+      result : result,
+      message : "Successfully User Profile has been created"
+    })
+  });
 });
 
 app.put("/api/user/edit/:id", (request, response) => {
-  
+  let id = request.params.id;
+
+  const firstName = request.body.first_name;
+  const lastName = request.body.last_name;
+  const email = request.body.email;
+  const contactNumber = request.body.contact_number;
+
+  const query = `UPDATE user_information SET first_name = '${firstName}', last_name = '${lastName}', email = '${email}', contact_number = ${contactNumber} WHERE id = ${id}`;
+
+  connection.query(query, (error, result) => {
+    if(error){
+      response.status(500).send(error);
+      return;
+    }
+
+    response.status(200).send({
+      result : result,
+      message : "Updated the user profile successfully"
+    })
+  });
 });
 
 app.delete("/api/user/delete/:id", (request, response) => {
